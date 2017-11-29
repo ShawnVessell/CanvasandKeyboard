d7bbdc22c9cf30bb472534222a08bb1ad058e67f
Shawn Vessell <shawn3x@gmail.com>
2017-11-28 18:48:48 -0600

    Second Commit

---
+++ CanvasandKeyboard/CanvasandKeyboard/Scripts/FileUploadScripts.js
@@ -0,0 +1,15 @@
+﻿function previewFile() {
+    var preview = document.querySelector('#imgPreview');
+    var file = document.querySelector('#fileButton').files[0];
+    var reader = new FileReader();
+
+    reader.onload = function () {
+        preview.src = reader.result;
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+    else {
+        preview.src = "";
+    }
+}
